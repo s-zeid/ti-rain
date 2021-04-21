16183edf704867e766c87059ff69fbf5c9d77e61
S. Zeid <s@zeid.me>
2021-04-21 04:11:00 -0500

    Reorganize JS; pass host element to constructor; add hideIfEmbedded field

--- rain.js
+++ rain.js
@@ -2,100 +2,86 @@ class Rain {
  // Number of raindrops (<=100)
  get R() { return 35; }
  
- constructor() {
-  this.params = this.canvas = this.ctx = this.scaleFactor = null;
+ // Hide the given element(s) if the animation is embedded.
+ // The value may be an array of Elements, a single Element, or null.
+ get hideIfEmbedded() {
+  return this._hideIfEmbedded || [];
+ }
+ set hideIfEmbedded(value) {
+  value = (value ? ((value instanceof Element) ? [value] : value) : []);
+  const oldValue = this.hideIfEmbedded;
+  this._hideIfEmbedded = value;
+  for (const el of oldValue) {
+   el.hidden = false;
+  }
+  if (this.params["embed"]) {
+   for (const el of value) {
+    el.hidden = true;
+   }
+  }
+ }
+ 
+ // Start the animation and add it to the given host element.
+ constructor(host) {
+  if (!(host instanceof Element))
+   throw new TypeError("host must be an element");
   
-  // Animation state
-  this.B = null;
+  // Instance state
+  this.params = this.constructor.getParams();
+  this.canvas = document.createElement("canvas");
+  this.ctx = this.canvas.getContext("2d");
+  this.scaleFactor = 0;
+  
+  // Animation state (nulls are unused)
+  this.B = 0; // BG Number
   this.C = null;
   this.D = null;
   this.E = null;
   this.F = null;
   this.G = null;
-  this.I = null;
-  this.N = null;
+  this.I = 1; // Initial run?
+  this.N = 0; // Raindrop+1 to move down
   this.S = null;
-  this.T = null;
+  this.T = 0; // Timer
   this.X = null;
   this.Y = null;
   this.Pic = null;
   this.Pic0 = null;
   this.Pic99 = null;
-  this.GDB0 = null;
-  this.GDB1 = null;
+  this.GDB0 = new Array(100);  // X coordinates (0-108)
+  this.GDB1 = new Array(100);  // Y coordinates (0-76)
+  
+  // Set initial coordinates
+  for (this.A=0;this.A<this.R;this.A++) {
+   this.GDB0[this.A] = this.constructor.randomInt(109);
+   this.GDB1[this.A] = this.constructor.randomInt(77);
+  }
+  
+  this.L = 15; // BG delay
+  this.M = 50; // FG delay
   
   // Animation options
   this.bgVersion = 2;
   
-  this.main();
- }
- 
- static getParams() {
-  const result = {};
-  const hash = location.hash.replace(/^#/, "");
-  for (let [k, v] of new URLSearchParams(hash)) {
-   if (v.toLowerCase() === "true" || v.length === 0)
-    v = true;
-   else if (v.toLowerCase() === "false")
-    v = false;
-   result[k] = v;
-  }
-  return result;
- }
- 
- // Determines the screen size.
- setScreenSize() {
-  const viewportWidth = window.innerWidth;
-  const viewportHeight = window.innerHeight;
-  if (typeof viewportWidth != "undefined" && typeof viewportHeight != "undefined") {
-   this.scaleFactor = Math.floor(viewportHeight / 64);
-   if ((96 * this.scaleFactor) > viewportWidth) {
-    this.scaleFactor = Math.floor(viewportWidth / 96);
-   }
-  }
-  else {
-   this.scaleFactor = 3;
-  }
-  const width = 96 * this.scaleFactor;
-  const height = 64 * this.scaleFactor;
-  this.canvas.width = width;
-  this.canvas.height = height;
-  this.canvas.style.width = String(width) + "px";
-  this.canvas.style.height = String(height) + "px";
- }
- 
- // Returns a random integer from 0 to maximum.
- static randomInt(maximum) {
-  return Math.floor(Math.random() * (maximum));
- }
- 
- // Clears the entire canvas.
- clearScreen() {
-  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
- }
- 
- // Draws a rectangle, scaling it and its position to correspond to the canvas
- // size.
- drawRect(x, y, w, h) {
-  this.ctx.fillRect(
-   x * this.scaleFactor, y * this.scaleFactor,
-   w * this.scaleFactor, h * this.scaleFactor,
-  );
- }
- 
- // Main function
- main() {
+  // Setup host environment
+  this.canvas.hidden = true;
+  host.appendChild(this.canvas);
   window.addEventListener("resize", this.setScreenSize.bind(this));
-  
-  this.params = this.constructor.getParams();
   if (window.top === window)
    window.addEventListener("hashchange", () => location.reload());
   
   // Setup canvas
-  this.canvas = document.getElementById("screen");
-  this.setScreenSize();
-  this.ctx = this.canvas.getContext("2d");
-  this.ctx.mozImageSmoothingEnabled = false;
+  this.canvas.setAttribute("data-rain", "");
+  if (this.canvas instanceof HTMLCanvasElement) {
+   this.canvas.width = 96;
+   this.canvas.height = 64;
+   this.setScreenSize();
+   this.ctx.mozImageSmoothingEnabled = false;
+  }
+  this.canvas.innerHTML = `
+   Please upgrade your browser to one that supports the HTML5 Canvas element.
+  `.trim();
+  this.canvas.hidden = false;
   
   // Handle embed mode if requested
   if (this.params["embed"]) {
@@ -103,7 +89,6 @@ class Rain {
    this.canvas.style.color = "black";
    document.body.style.background = "transparent";
    document.documentElement.style.background = "transparent";
-   document.querySelector("aside").style.display = "none";
   }
   
   // Set runtime canvas options
@@ -139,34 +124,56 @@ class Rain {
     this.canvas.parentElement.insertBefore(fallbackImg, this.canvas);
     this.canvas.remove();
     this.canvas = fallbackImg;
-    this.canvas.setAttribute("id", "screen");
+    this.canvas.setAttribute("data-rain", "");
     this.setScreenSize();
     if (this.params["embed"])
      this.canvas.style.background = "transparent";
    }
   }
   
-  // Initialize X (0-108) and Y (0-76) coordinate arrays
-  this.GDB0 = new Array(100);
-  this.GDB1 = new Array(100);
-  
-  // Set initial coordinates
-  for (this.A=0;this.A<this.R;this.A++) {
-   this.GDB0[this.A] = this.constructor.randomInt(109);
-   this.GDB1[this.A] = this.constructor.randomInt(77);
-  }
-  
-  this.B = 0; // BG Number
-  this.T = 0; // Timer
-  this.N = 0; // Raindrop+1 to move down
-  this.I = 1; // Initial run?
-  this.L = 0; // Initialize
-  this.M = 0; // delays
-  
-  this.L = 15; // BG Delay = L
-  this.M = 50; // FG Delay = M
-  
   setInterval(this.loop.bind(this), this.L);
+ }
+ 
+ // Gets hash parameters, formatted as if query string parameters.
+ // Values of "true" and "false" will be converted to Booleans.
+ // Parameters with no value will be set to the Boolean value true.
+ static getParams(hash) {
+  const result = {};
+  hash = (hash || location.hash).replace(/^#/, "");
+  for (let [k, v] of new URLSearchParams(hash)) {
+   if (v.toLowerCase() === "true" || v.length === 0)
+    v = true;
+   else if (v.toLowerCase() === "false")
+    v = false;
+   result[k] = v;
+  }
+  return result;
+ }
+ 
+ // Returns a random integer from 0 to maximum.
+ static randomInt(maximum) {
+  return Math.floor(Math.random() * (maximum));
+ }
+ 
+ // Determines the screen size.
+ setScreenSize() {
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
+  if (typeof viewportWidth != "undefined" && typeof viewportHeight != "undefined") {
+   this.scaleFactor = Math.floor(viewportHeight / 64);
+   if ((96 * this.scaleFactor) > viewportWidth) {
+    this.scaleFactor = Math.floor(viewportWidth / 96);
+   }
+  }
+  else {
+   this.scaleFactor = 3;
+  }
+  const width = 96 * this.scaleFactor;
+  const height = 64 * this.scaleFactor;
+  this.canvas.width = width;
+  this.canvas.height = height;
+  this.canvas.style.width = String(width) + "px";
+  this.canvas.style.height = String(height) + "px";
  }
  
  // Draws the background and appropriate raindrops.
@@ -211,6 +218,20 @@ class Rain {
     this.I = 0;
    }
   }
+ }
+ 
+ // Clears the entire canvas.
+ clearScreen() {
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+ }
+ 
+ // Draws a rectangle, scaling it and its position to correspond to the canvas
+ // size.
+ drawRect(x, y, w, h) {
+  this.ctx.fillRect(
+   x * this.scaleFactor, y * this.scaleFactor,
+   w * this.scaleFactor, h * this.scaleFactor,
+  );
  }
  
  // Draws a raindrop, scaling it and its position to correspond to the canvas
